88bf1e7c77516626a68b260751403481021098d8
waltindobne <145157901+waltindobne@users.noreply.github.com>
2023-12-06 17:29:03 -0300

    recoloquei o         public required string Senha { get; set; }

--- catalogo_carros/Models/Usuario.cs
+++ catalogo_carros/Models/Usuario.cs
@@ -7,7 +7,7 @@
         public required string Nome { get; set; }
 
         public required string NomeUsuario { get; set; }
-         
 
+        public required string Senha { get; set; }
     }
 }
